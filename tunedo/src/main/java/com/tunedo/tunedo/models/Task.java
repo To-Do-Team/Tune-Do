c2b715a53eebd8b32bcc1ab68a15b68ae7520750
ZBishop <obisporicapa@gmail.com>
2024-07-22 18:59:05 -0500

    TDT-18 Base, Task, Type, User

---
+++ tunedo/src/main/java/com/tunedo/tunedo/models/Task.java
@@ -0,0 +1,41 @@
+package com.tunedo.tunedo.models;
+
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "tasks")
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Task extends BaseModel{
+    
+    @NotNull
+    @NotEmpty
+    @Size(min = 3)
+    private String title;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3)
+    private String description;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3)
+    private String notes;
+
+    private Instant deadline;
+    
+    private Status status;
+
+    private Type Type;
+}
