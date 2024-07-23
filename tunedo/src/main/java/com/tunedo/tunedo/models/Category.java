8f6f212d26727a3816a0b1af3593e3f3446fa3ea
piero <pierog@hotmail.es>
2024-07-22 23:18:46 -0500

    TDT-18 Added Cat-Rol-Song

--- tunedo/src/main/java/com/tunedo/tunedo/models/Category.java
+++ tunedo/src/main/java/com/tunedo/tunedo/models/Category.java
@@ -1,5 +1,29 @@
 package com.tunedo.tunedo.models;
 
-public class Category {
-    
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category extends BaseModel {
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3)
+    private String name;
+       
+    @NotNull
+    @NotEmpty
+    @Size(min = 10)
+    private String description;
+
 }
