832588109a0aa7c1d6a07251cb1226f7d0d0292e
ZBishop <obisporicapa@gmail.com>
2024-07-22 19:52:43 -0500

    TDT-18-Status
    Add enum for Status

---
+++ tunedo/src/main/java/com/tunedo/tunedo/models/Status.java
@@ -0,0 +1,16 @@
+package com.tunedo.tunedo.models;
+
+import lombok.Getter;
+
+@Getter
+public enum Status {
+    ToDo("Por hacer"),
+    Doing("Haciendo"),
+    Done("Hechos");
+    
+    private String description;
+
+    Status(String description){
+        this.description=description;
+    }
+}
