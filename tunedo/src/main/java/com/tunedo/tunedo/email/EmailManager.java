be4981ca56e010973b2e408111dadc18add7529d
ZBishop <obisporicapa@gmail.com>
2024-07-25 23:53:01 -0500

    TDT-72-Email init
    This is required in Spring boot for a mail sender using SMTP

---
+++ tunedo/src/main/java/com/tunedo/tunedo/email/EmailManager.java
@@ -0,0 +1,23 @@
+package com.tunedo.tunedo.email;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailManager {
+    private final JavaMailSender mailSerder;
+
+    public EmailManager(JavaMailSender mailSerder) {
+        this.mailSerder = mailSerder;
+    }
+
+    public void sendEmail(String to,String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("janedoecd2024@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSerder.send(message);
+    }
+}
