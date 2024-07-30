116ba9735c67118eafa9e7a77883492c7a5f53e0
ZBishop <obisporicapa@gmail.com>
2024-07-30 12:01:29 -0500

    TDT-70-Controller Add Cat-Add-Task
    This function adds another category to a existing task with one or more categories

--- tunedo/src/main/java/com/tunedo/tunedo/services/CategoryService.java
+++ tunedo/src/main/java/com/tunedo/tunedo/services/CategoryService.java
@@ -1,8 +1,12 @@
 package com.tunedo.tunedo.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.tunedo.tunedo.models.Category;
+import com.tunedo.tunedo.models.Task;
 import com.tunedo.tunedo.repositories.BaseRepository;
 
 @Service
@@ -10,5 +14,15 @@ public class CategoryService extends BaseService<Category> {
 
     public CategoryService(BaseRepository<Category> repository) {
         super(repository);
+    }
+
+    public List<Category> addAnotherCategory(Long id, Task task){
+        Category category= findById(id);
+        List<Category> categories = new ArrayList<>();
+        if(task.getCategories()!=null){
+            categories.addAll(task.getCategories());
+        }
+        categories.add(category);
+        return categories;
     }
 }
