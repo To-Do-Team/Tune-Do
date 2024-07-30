74e9e8cb8fb7ed893cf5b84463dc06195eb11299
ZBishop <obisporicapa@gmail.com>
2024-07-30 12:47:37 -0500

    Home Basic View
    Added css
    Added js logic
    Must show and let you move tasks that updates in DB

---
+++ tunedo/src/main/resources/static/js/drag.js
@@ -0,0 +1,134 @@
+const draggables = document.querySelectorAll(".task");
+const droppables = document.querySelectorAll(".swim-lane");
+let tasks;
+document.addEventListener('DOMContentLoaded', function() {
+    // Para hacer la comparación
+    tasks= tasksData
+    console.log(tasksData);
+});
+let allListTasks = null;
+let bottomTask =null;
+let topTask =null;
+let previousBottomTask= null;
+let newZone =null;
+let previousZone =null;
+draggables.forEach((task) =>{
+    task.addEventListener("dragstart",(e)=>{
+        task.classList.add("is-dragging");
+        previousBottomTask=e.currentTarget.nextElementSibling;
+        previousZone=e.target.parentElement.dataset.type;
+    });
+    task.addEventListener("dragend",()=>{
+        task.classList.remove("is-dragging");
+        if(bottomTask===previousBottomTask && newZone===previousZone){
+            return;
+        }
+        calculatePosition(task,topTask,bottomTask);
+    })
+});
+
+droppables.forEach((zone) =>{
+    zone.addEventListener("dragover",(e)=>{
+        e.preventDefault();
+        bottomTask = insertAboveTask(zone, e.clientY);
+        const currentTask = document.querySelector(".is-dragging");
+        if(!bottomTask){
+            zone.appendChild(currentTask);
+        }
+        else{
+            zone.insertBefore(currentTask,bottomTask,zone);
+        }
+        setBottomTopTasks(bottomTask, allListTasks);
+    });
+    zone.querySelectorAll(".task");
+});
+
+const insertAboveTask = (zone,mouseY)=>{
+    const elements= zone.querySelectorAll(".task:not(.is-dragging)");
+    let closestTask= null;
+    let closestOffset = Number.NEGATIVE_INFINITY;
+    
+    elements.forEach((task)=>{
+        const {top} = task.getBoundingClientRect();
+
+        const offset = mouseY - top;
+
+        if(offset <0 && offset > closestOffset){
+            closestOffset = offset;
+            closestTask=task;
+        }
+    });
+    allListTasks= [...elements];
+    newZone=zone.dataset.type;
+    //console.log("NUEVA ",newZone);
+    return closestTask;
+};
+
+const setBottomTopTasks = (closestTask,elements)=>{
+    bottomTask=closestTask;
+    if(elements.length<=0){
+        topTask=null;
+        bottomTask=null;
+        //console.log("no hay elementos");
+        return
+    }
+    const index =elements.indexOf(closestTask);
+    //console.log("indice ",index);
+    if(bottomTask==null && index<0){
+        topTask=elements[elements.length -1];
+        //console.log("lo puso al ultimo");
+        return
+    }else if(index>=1){
+        topTask=elements[index-1];
+        //console.log("lo puso en medio");
+        return
+    }
+        topTask=null;
+        //console.log("lo puso al principio");
+};
+
+const changePosition= (task,position)=>{
+    task.dataset.pos=position;
+    const editTask = {
+        id: task.id,
+        position: position,
+        type: newZone
+    };
+    fetch('/tasks/'+task.id+'/edit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editTask)
+    })
+    .then(response => response.json())
+    .then(data => 
+        console.log(data)
+        //tasks[data.type][data.id].position=data.position
+    )
+    .catch((error) => console.error('Error:', error));
+};
+
+const calculatePosition = (task,topTask,bottomTask)=>{
+    const indexStep = 65536 //2^16
+    let position =0;
+    if(topTask !== null || bottomTask !==null){//si no es la unica tarea: 
+        if(topTask===null){//esta primero?
+            position =  parseFloat(bottomTask.dataset.pos)/2;
+        }
+        else if(bottomTask ===null){
+            position = parseFloat(topTask.dataset.pos) +indexStep;
+        }
+        else{
+            position =parseFloat(topTask.dataset.pos) + parseFloat(bottomTask.dataset.pos);
+            position/=2;
+        }
+    }
+    else{
+        position=indexStep;
+    }
+    changePosition(task, position);
+    previousZone=null;
+    topTask=null;
+    bottomTask=null;
+};
--- tunedo/src/main/webapp/WEB-INF/home.jsp
+++ tunedo/src/main/webapp/WEB-INF/home.jsp
@@ -12,36 +12,42 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
+    <script src="/js/drag.js" defer></script>
+    <script>
+        var tasksData = JSON.parse('<c:out value="${tasksJson}" escapeXml="false"/>');
+    </script>
 </head>
 <body>
     <div class="d-flex justify-content-around my-3">
         <h6 class="text-red">Hola, <c:out value="${user.getName()}" /></h6>
         <a href="/?logout">Cerrar Sesión</a>
+        
+            <div class="d-flex justify-content-start my-5">
+                <a class="text-decoration-none text-light bg-danger border border-danger rounded px-2 py-1" href="tasks/new"><- Nueva tarea</a>
+            </div>
     </div>
-    <div class="d-flex flex-column align-items-evenly m-auto w-75">
-        <h2 class="text-primary">Todas tus tareas:</h2>
-        <div class="d-flex p-3 my-3 justify-content-between border-3 border-bottom border-secondary">
-            <h3>Tarea</h3>
-            <h3>Categorías</h3>
-        </div>
-        <c:forEach items="${tasks}" var="task">
-        <div class="d-flex justify-content-between my-3">
-            <div class="d-flex flex-column">
-                <blockquote class="blockquote">
-                <a class="display-6" href="tasks/${task.getId()}"><c:out value="${task.getTitle()}" /></a>
-                </blockquote>
-                <small>
-                    <c:out value="${task.getGenre()}" />
-                </small>
+    <div class="d-flex flex-wrap gap-3">
+        <c:forEach items="${tasksByType.entrySet()}" var="entry">
+            <h2 class="text-primary"><c:out value="${entry.getKey().getDescription()}" /></h2>
+            <div class="p-2 col-4 swim-lane" data-type="${entry.getKey().name()}">
+                <c:forEach items="${entry.getValue()}" var="task">
+                <div id="${task.id}" data-pos="${String.valueOf(task.getPosition())}" class="d-flex justify-content-between my-3 task" draggable="true">
+                    <div class="d-flex flex-column">
+                        <blockquote class="blockquote">
+                        <a class="display-6" href="tasks/${task.getId()}"><c:out value="${task.getTitle()}" /></a>
+                        </blockquote>
+                        <small>
+                            <c:out value="${task.getDescription()}" />
+                        </small>
+                    </div>
+                    <div class="w-25 justify-content-end">
+                        <h2 class="text-center"><c:out value="${task.getCategories().size()}" /></h2>
+                    </div>
+                </div>
+                </c:forEach>
             </div>
-            <div class="w-25 justify-content-end">
-                <h2 class="text-center"><c:out value="${task.getCategories().size()}" /></h2>
-            </div>
-        </div>
         </c:forEach>
-        <div class="d-flex justify-content-start my-5">
-            <a class="text-decoration-none text-light bg-danger border border-danger rounded px-2 py-1" href="tasks/new"><- Nueva tarea</a>
-        </div>
+        
     </div>
 </body>
 </html>
